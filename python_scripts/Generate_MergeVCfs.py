7fc1540f82bb67b18c36866cb2c6d04dc5d3ad7a
durwa004 <durwa004@umn.edu>
2021-12-01 14:29:12 -0600

    Created for Emma

---
+++ python_scripts/Generate_MergeVCfs.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  5 11:04:17 2019
+
+@author: jonahcullen
+"""
+
+import argparse
+import os
+
+
+def make_arg_parser():
+    parser = argparse.ArgumentParser(
+            prog="GeneratePBS.py",
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+            "-d", "--data",
+            default=argparse.SUPPRESS,
+            metavar="",
+            required=True,
+            help="Path to dir containing the ibio output files [required]")
+    parser.add_argument(
+            "-c", "--chrs",
+            default=argparse.SUPPRESS,
+            metavar="",
+            required=True,
+            help="Path to directory containing group gvcfs [required]")
+    return parser
+
+
+if __name__ == '__main__':
+     
+    parser = make_arg_parser()
+    args = parser.parse_args()
+
+    data = os.path.abspath(args.data)
+    chr_ids = os.path.abspath(args.chrs)
+    
+    java = 'java -Xmx4g -jar /home/mccuem/shared/.local/conda/envs/HorseGenomeProject/bin/picard.jar MergeVcfs'
+ 
+    header = (
+              "#!/bin/bash -l\n"  
+              "#SBATCH --time=12:00:00\n"
+              "#SBATCH --ntasks=1\n"
+              "#SBATCH --mem=2g\n"
+              "#SBATCH --mail-type=ALL\n"
+              "#SBATCH --mail-user=perso208@umn.edu\n"
+              "#SBATCH -o mergevcfs.out\n"
+              "#SBATCH -e mergevcfs.err\n"
+              "#SBATCH -p small,amdsmall\n"
+             )
+    
+    with open(f"{chr_ids}/vcfs_for_merging.list", "w") as f:
+        for file_name in os.listdir(chr_ids):
+            if file_name.endswith(".vcf"):
+                print(file_name, file=f)
+    
+    pbs = os.path.join(os.getcwd(), "MergeVCFs.slurm")
+    
+    with open(pbs, "w") as f:
+        print(header, file=f)
+        print(f"cd {chr_ids}\n", file=f)
+        print(f"{java} I=vcfs_for_merging.list O=merged_canine_vcf.vcf.gz", file=f)
+        
